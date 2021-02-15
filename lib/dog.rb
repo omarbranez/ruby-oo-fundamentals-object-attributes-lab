99f657a4453d3fb3376a8c85d41b988c03ab8c5e
Omar Branez <omar.branez@gmail.com>
2021-02-15 13:16:39 -0500

    commit tree 86b74a7891c76a5b9afc96965778a44f75c546c5

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+    attr_accessor :name, :breed #att_accessor works to initialize both without the def initialize
+
+    # def name
+    #     @name
+    # end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+attr_accessor :name, :job
+
+    # def name
+    #     @name
+    # end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def job
+    #     @job
+    # end
+
+    # def job=(job)
+    #     @job = job
+    # end
+end
